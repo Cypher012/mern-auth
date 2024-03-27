1cfcf59caaa5b30164cc3a04243f0c293e320db7
Blaqcipher <ayojoade@gmail.com>
2024-03-27 17:54:56 +0100

    create test api route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./connectDB.js";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const port = process.env.PORT;
 const URI = process.env.URI;
 
 connectDB(URI);
+
+app.use("/api/user", userRoutes);
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
