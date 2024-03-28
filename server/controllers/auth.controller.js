27fe42e752a2c6b4d0db943b938dd89ea15cc9f7
Blaqcipher <ayojoade@gmail.com>
2024-03-28 16:20:22 +0100

    Create sign in api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,10 @@
 import User from "../model/user.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -11,6 +16,27 @@ export const signUp = async (req, res, next) => {
       password: hashedPassword,
     }).save();
     res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(401, "User not found"));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 3600000), //1 hour
+      })
+      .status(200)
+      .json({ rest });
   } catch (error) {
     next(error);
   }
